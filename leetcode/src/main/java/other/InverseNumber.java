b5082a9d4daf0c95d31e8c4531e3bc318210e38d
wangzzu <wangm92@163.com>
2016-04-25 23:11:19 +0800

    InverseNumber

--- leetcode/src/main/java/other/InverseNumber.java
+++ leetcode/src/main/java/other/InverseNumber.java
@@ -6,25 +6,49 @@ package other;
  */
 public class InverseNumber {
 	public static void main(String[] args) throws InterruptedException {
+		int[] a={7,5,6,4};
+		System.out.println(inverseNumber(a,0,3));
 	}
 
-//	public  static int inverseNumber(int[] a, int start, int end){
-//		int num=0;
-//		if(end-start+1==0){
-//			return num;
-//		}
-//		int mid=(end-start+1)/2;
-//		int left=inverseNumber(a,start,mid);
-//		int right=inverseNumber(a,mid+1,end);
-//		while (mid+1<=end){
-//			int tmp=a[mid+1];
-//			for(int i=0;i<mid+1-start;i++){
-//				if(a[start+i]>tmp){
-//					num+=mid-mid-start-i+1;
-//
-//				}
-//			}
-//			mid++;
-//		}
-//	}
+	public static int inverseNumber(int[] a, int start, int end) {
+		int num = 0;
+		if (start == end) {
+			return num;
+		}
+		int mid = (end - start + 1) / 2-1+start;
+		int left = inverseNumber(a, start, mid);
+		int right = inverseNumber(a, mid + 1, end);
+		int i = mid;
+		int j = end;
+		while (i >= start && j >= mid + 1) {
+			if (a[i] > a[j]) {
+				num += j - mid;
+				i--;
+			} else {
+				j--;
+			}
+		}
+		merge(a,start,mid,end);
+		return left+right+num;
+	}
+
+	public static void merge(int[] a, int start, int mid, int end) {
+		int i = start;
+		int j = mid + 1;
+		int[] ao = new int[a.length];
+		for (int k = start; k <= end; k++) {
+			ao[k] = a[k];
+		}
+		for (int k = start; k <= end; k++) {
+			if (i > mid) {
+				a[k] = ao[j++];
+			} else if (j > end) {
+				a[k] = ao[i++];
+			} else if (ao[i] > ao[j]) {
+				a[k] = ao[j++];
+			} else {
+				a[k] = ao[i++];
+			}
+		}
+	}
 }
